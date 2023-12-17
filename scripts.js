bcf32f753f49c2ea54d64218724b5ae1d8aff308
viniciossobral <msobral71@gmail.com>
2023-12-17 16:58:00 -0300

    Projeto: Conversão de Moedas

---
+++ scripts.js
@@ -0,0 +1,37 @@
+const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
+
+function convertValues() {
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") // Valor em Real
+    const currencyValueConverted = document.querySelector(".currency-value") // Outras moedas
+
+    const dolarToday = 5.2
+    const euroToday = 6.2
+    const libraToday = 7.2
+
+    if (currencySelect.value == "dolar") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+    }
+
+    if (currencySelect.value == "euro") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euroToday)
+    }
+
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+
+
+}
+
+convertButton.addEventListener("click", convertValues)
